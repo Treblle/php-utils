2d3e3b73756e211252512957ab85e72a338475ef
Christopher Miller <chris@treblle.com>
2023-08-30 13:55:53 +0100

    ehancing checks around auth headers

--- tests/Masking/FieldMaskerTest.php
+++ tests/Masking/FieldMaskerTest.php
@@ -55,3 +55,116 @@ it('can mask an recursive array', function (): void {
         'foo' => 'bar',
     ]);
 });
+
+it('can handle a single Authorization entry', function () {
+    $masker = new FieldMasker(
+        fields: ['password', 'api_key', 'cc'],
+    );
+
+    expect($masker->mask(
+        data: [
+            'form' => [
+                'password' => 'password',
+                'api_key' => 'test',
+            ],
+            'Authorization' => '123123123123123',
+            'X-API-KEY' => '1234-1234-4321',
+            'cc' => '1234-1234-1234-1234',
+            'foo' => 'bar',
+        ],
+    ))->toBeArray()->toEqual([
+        'form' => [
+            'password' => '********',
+            'api_key' => '****',
+        ],
+        'Authorization' => '***************',
+        'X-API-KEY' => '**************',
+        'cc' => '*******************',
+        'foo' => 'bar',
+    ]);
+});
+
+it('can handle a two Authorization entries', function () {
+    $masker = new FieldMasker(
+        fields: ['password', 'api_key', 'cc'],
+    );
+
+    expect($masker->mask(
+        data: [
+            'form' => [
+                'password' => 'password',
+                'api_key' => 'test',
+            ],
+            'Authorization' => 'Bearer 123123123123123',
+            'X-API-KEY' => '1234-1234-4321',
+            'cc' => '1234-1234-1234-1234',
+            'foo' => 'bar',
+        ],
+    ))->toBeArray()->toEqual([
+        'form' => [
+            'password' => '********',
+            'api_key' => '****',
+        ],
+        'Authorization' => 'Bearer ***************',
+        'X-API-KEY' => '**************',
+        'cc' => '*******************',
+        'foo' => 'bar',
+    ]);
+});
+
+
+it('can handle a multiple Authorization entries', function () {
+    $masker = new FieldMasker(
+        fields: ['password', 'api_key', 'cc'],
+    );
+
+    expect($masker->mask(
+        data: [
+            'form' => [
+                'password' => 'password',
+                'api_key' => 'test',
+            ],
+            'Authorization' => 'Bearer 123123123123123 123',
+            'X-API-KEY' => '1234-1234-4321',
+            'cc' => '1234-1234-1234-1234',
+            'foo' => 'bar',
+        ],
+    ))->toBeArray()->toEqual([
+        'form' => [
+            'password' => '********',
+            'api_key' => '****',
+        ],
+        'Authorization' => 'Bearer *************** ***',
+        'X-API-KEY' => '**************',
+        'cc' => '*******************',
+        'foo' => 'bar',
+    ]);
+});
+
+it('can handle a malformed Authorization entry', function () {
+    $masker = new FieldMasker(
+        fields: ['password', 'api_key', 'cc'],
+    );
+
+    expect($masker->mask(
+        data: [
+            'form' => [
+                'password' => 'password',
+                'api_key' => 'test',
+            ],
+            'Authorization' => 'Alien',
+            'X-API-KEY' => '1234-1234-4321',
+            'cc' => '1234-1234-1234-1234',
+            'foo' => 'bar',
+        ],
+    ))->toBeArray()->toEqual([
+        'form' => [
+            'password' => '********',
+            'api_key' => '****',
+        ],
+        'Authorization' => '*****',
+        'X-API-KEY' => '**************',
+        'cc' => '*******************',
+        'foo' => 'bar',
+    ]);
+});
